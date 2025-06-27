c89028c7ac5ad35590079790b84a9393609ae0a1
Vladpru <vladllen96681@gmail.com>
2025-06-27 17:46:05 +0300

    small change

--- src/components/ui/AdditionalOptions.tsx
+++ src/components/ui/AdditionalOptions.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 import { Audiowide } from 'next/font/google';
 import addOptions from '../../data/additionals';
